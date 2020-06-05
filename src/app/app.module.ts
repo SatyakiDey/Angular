663bc3257576951c1abe098b0a3c27da7f17d35b
Satyaki Dey <satyakidey11@gmail.com>
2020-06-05 21:22:29 +0530

    Basics of Angular Services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,8 @@ import  {MatGridListModule} from '@angular/material/grid-list';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import {DishService} from './services/dish.service';
+import { Dish } from './shared/dish';
 @NgModule({
   declarations: [
     AppComponent,
@@ -29,7 +31,9 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MatCardModule,
     MatButtonModule
   ],
-  providers: [],
+  providers: [
+    DishService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -3,7 +3,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Dish} from 'src/app/shared/dish'; //or '../shared/dish'
 
-import {DISHES} from '../shared/dishes';
+import {DishService} from '../services/dish.service';
 
 @Component({
   selector: 'app-menu',
@@ -11,11 +11,12 @@ import {DISHES} from '../shared/dishes';
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements OnInit {
-  dishes:Dish[]=DISHES;
+  dishes:Dish[];
   selectedDish :Dish ;
-  constructor() { }
+  constructor(private dishService: DishService) { } //The "dishService" variable is created when the menu component is instantiated. This variable is of type "DishService" which is a class of the "dish.service" service module.
 
   ngOnInit() {
+    this.dishes=this.dishService.getDishes();  //This is a life cycle method. This method fetches relevant information from the service using the variable we declared in the constructor. "getDishes()" is a method of the service.
   }
 onSelect(dish:Dish){
   this.selectedDish=dish;
