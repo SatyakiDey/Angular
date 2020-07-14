f62eafa1c9f2ad570b50ed3cba4cd91729b983c4
Satyaki Dey <satyakidey11@gmail.com>
2020-07-14 11:17:36 +0530

    Angular Reactive Forms Part 3

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -15,6 +15,34 @@ export class ContactComponent implements OnInit {
 
   @ViewChild('fform') feedbackFormDirective; //this is used to access DOM elememts of the template. We can obtain references to template elements and have them injected into the our Component class by querying the template: that's what @ViewChild is for.
 
+  formErrors={
+    'firstname':'',
+    'lastname':'',
+    'telnum':'',
+    'email':''
+  };
+
+  validationMessages={
+    'firstname':{
+      'required':'First name is required.',
+      'minlength':'First name must be atleast 2 characters long.',
+      'maxlength':'First name cannot be more than 25 characters.'
+    },
+    'lastname':{
+      'required':'Last name is required.',
+      'minlength':'Last name must be atleast 2 characters long.',
+      'maxlength':'Last name cannot be more than 25 characters.'
+    },
+    'telnum':{
+      'required':'Telephone number is required.',
+      'pattern':'Telephone number must contain only numbers.'
+    },
+    'email':{
+      'required':'Email is required.',
+      'email':'Email not in valid format.'
+    }
+  };
+
   constructor(private fb:FormBuilder) {  //formBuilder is used to create formGroups and use formControl. Creating it inside a constructor means that it'll get created when the class is created(Dependency Injection).
 
     this.createForm(); //used to create the form according to Angular's form styling guidelines.
@@ -28,14 +56,44 @@ export class ContactComponent implements OnInit {
       //Making the feedbackForm equal to the returned value of the 'group' method of FormBuilder class.
 
       this.feedbackForm=this.fb.group({
-        firstname:['',Validators.required],
-        lastname:['',Validators.required],
-        telnum:[0,Validators.required],
-        email:['',Validators.required],
+        firstname:['',[Validators.required,Validators.minLength(2),Validators.maxLength(25)]],
+        lastname:['',[Validators.required,Validators.minLength(2),Validators.maxLength(25)]],
+        telnum:[0,[Validators.required,Validators.pattern]],
+        email:['',[Validators.required,Validators.email]],
         agree:false,
         contacttype:'',
         message:''
-      })
+      });
+
+      this.feedbackForm.valueChanges
+      .subscribe(data => this.onValueChanged(data)); //data returned by the Observable is sent as a parameter to the 'onValueChanaged()' method
+
+      this.onValueChanged(); //reset form validation messages
+    }
+
+    onValueChanged(data?:any){
+      if(!this.feedbackForm){return;}
+      const form=this.feedbackForm;
+      for(const field in this.formErrors){
+        if(this.formErrors.hasOwnProperty(field)){
+          //clearing previous error messages(if any)
+          this.formErrors[field]='';
+          //getting firstname/lastname/telnum/email attributes form the form one at a time.
+          const control=form.get(field);
+          //checking the condition of the concerned input field by their formControlName.
+          if(control && control.dirty && !control.valid){ 
+            //Getting the appropriate Validation message from the 'validationMessages' object of the concerned input field using their formControlName. 
+            const messages=this.validationMessages[field];
+            //Getting all the errors of a concerned input field. 
+            for(const key in control.errors){
+              if(control.errors.hasOwnProperty(key)){
+                //Concatinating each error after previous for a particular form attribute and storing it the appropriate field attribute of the object. 
+                this.formErrors[field] += messages[key]+' ';
+              }
+            }
+          }
+        }
+      }
     }
 
     onSubmit(){
